fee54697aa5afb61f090ee39a6c7c99d0b8bf4be
dinoli <dino.li@mail.utoronto.ca>
2019-10-30 23:50:39 -0400

    Add the icon setting

--- sickkids_uroflow/RecordPage.js
+++ sickkids_uroflow/RecordPage.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {Component} from "react";
 import { Text, View } from "react-native";
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class RecordPage extends React.Component {
     render() {
@@ -12,6 +14,13 @@ class RecordPage extends React.Component {
                 }}
             >
                 <Text>Record Page</Text>
+                <Button icon={
+                    <Icon name="md-checkmark-circle" size={125} color="white"/>
+                }
+  title="Button with icon component"
+  onPress = {() => {console.log(1)}}
+/>
+
             </View>
         );
     }
