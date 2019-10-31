74c3edcc09eca6eeb630709d4a5e9f08952a9082
George Huang <georgeh9897@gmail.com>
2019-10-31 14:36:50 -0400

    Stash

--- sickkids_uroflow/RecordPage.js
+++ sickkids_uroflow/RecordPage.js
@@ -1,7 +1,5 @@
-import React, {Component} from "react";
+import React from "react";
 import { Text, View } from "react-native";
-import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 class RecordPage extends React.Component {
     render() {
@@ -14,13 +12,6 @@ class RecordPage extends React.Component {
                 }}
             >
                 <Text>Record Page</Text>
-                <Button icon={
-                    <Icon name="md-checkmark-circle" size={125} color="white"/>
-                }
-  title="Button with icon component"
-  onPress = {() => {console.log(1)}}
-/>
-
             </View>
         );
     }
