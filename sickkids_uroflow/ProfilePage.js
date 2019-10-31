7b2cb783faeafb9fb17470e8a9d8d5158582f957
Youxuan Ma <43625118+youxuan-ma@users.noreply.github.com>
2019-10-31 19:33:01 -0400

    simple demonstration for basic inputs
    buttons, inputbox, select,text ,modifiy keypads

--- sickkids_uroflow/ProfilePage.js
+++ sickkids_uroflow/ProfilePage.js
@@ -1,17 +1,54 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button, Picker,TextInput} from "react-native";
+
+function fake() {
+
+}
 
 class ProfilePage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {gender: null,
+                        height: null}
+    }
     render() {
         return (
             <View
                 style={{
                     flex: 1,
                     justifyContent: "center",
-                    alignItems: "center"
+                    textAlign: "left"
                 }}
             >
                 <Text>Profile Page</Text>
+                <Text>gender:</Text>
+                <Picker
+                  selectedValue={this.state.gender}
+                  style={{ height: 50, width: 300 }}
+                  onValueChange={(itemValue, itemIndex)=> this.setState({ gender: itemValue })}>
+                  <Picker.Item label="Male" value="Male" />
+                  <Picker.Item label="Female" value="Female" />
+                </Picker>
+                <Text>height (in cm):</Text>
+                <TextInput
+                        style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 5 }}
+                        autoCompleteType='cc-number'
+                        keyboardType='number-pad'
+                        onChangeText={height => this.setState({ height })}
+                        value={this.state.height}
+                      />
+                <Button
+                      onPress={fake}
+                      title="submit"
+                      color="#841584"
+                      accessibilityLabel="submit"
+                />
+                <Button
+                      onPress={fake}
+                      title="logoff"
+                      color="red"
+                      accessibilityLabel="logoff"
+                />
             </View>
         );
     }
