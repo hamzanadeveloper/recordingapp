eea90ea70303d9c386b59366fe4464dce42ededc
George Huang <georgeh9897@gmail.com>
2019-10-31 22:54:43 -0400

    Add additional set of possible recorder code in RecordPage; Add simple layout for ResultPage

--- sickkids_uroflow/RecordPage.js
+++ sickkids_uroflow/RecordPage.js
@@ -4,6 +4,9 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import { Ionicons } from "@expo/vector-icons";
 import { Stopwatch, Timer} from 'react-native-stopwatch-timer';
 import { NavigationEvents } from "react-navigation";
+import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+import * as Permissions from 'expo-permissions'
+
 // import AudioRecord from 'react-native-audio-record';
 // import Recorder from 'recorder-js';
 
@@ -27,6 +30,7 @@ import { NavigationEvents } from "react-navigation";
  
 // let isRecording = false;
 // let blob = null;
+// const audioRecorderPlayer = new AudioRecorderPlayer();
 
 class RecordPage extends React.Component {
     constructor(props){
@@ -35,16 +39,51 @@ class RecordPage extends React.Component {
             isStopwatchStart:false,
             resetStopwatch:false,
             timerDuration:90000,
+            recordingPermission:false,
         };
         // this.temp = AudioRecord.init(options);
         this.startStopStopWatch = this.startStopStopWatch.bind(this);
         this.resetStopwatch = this.resetStopwatch.bind(this);
+        this.audioRecorderPlayer = new AudioRecorderPlayer();
     }
 
     // navigator.mediaDevices.getUserMedia({audio: true})
     // .then(stream => recorder.init(stream))
     // .catch(err => console.log('Uh oh... unable to get stream...', err));
-
+    componentDidMount(){
+        this._askForPermission()
+    }
+    
+    _askForPermission = async () => {
+        const response = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+        this.setState({
+            recordingPermission: response.status === 'granted',
+        })
+    }
+    
+    // Recorder code for react-native-audio-recorder-player!!!!!
+    // onStartRecord = async () => {
+    //     const result = await this.audioRecorderPlayer.startRecorder();
+    //     this.audioRecorderPlayer.addRecordBackListener((e) => {
+    //       this.setState({
+    //         recordSecs: e.current_position,
+    //         recordTime: audioRecorderPlayer.mmssss(
+    //           Math.floor(e.current_position),
+    //         ),
+    //       });
+    //       return;
+    //     });
+    //     Alert.alert("Record Start!!!!!");
+    // };
+      
+    // onStopRecord = async () => {
+    //     const result = await this.audioRecorderPlayer.stopRecorder();
+    //     this.audioRecorderPlayer.removeRecordBackListener();
+    //     this.setState({
+    //       recordSecs: 0,
+    //     });
+    //     console.log("Record stopped!!!!!!");
+    // };
 
     startStopStopWatch(){
         this.setState({
@@ -54,10 +93,12 @@ class RecordPage extends React.Component {
         console.log(this.state.isStopwatchStart)
         if (this.state.isStopwatchStart){
             // this.temp.start();
+            // this.onStartRecord();
             return Alert.alert("Record Start")
         } else {
             // audioFile = await this.temp.stop();
             // console.log(audioFile);
+            // this.onStopRecord();
             return Alert.alert(
                 "Stopped Recording",
                 "See Result?",
@@ -129,7 +170,7 @@ class RecordPage extends React.Component {
                     alignItems: "center"
                 }}
             >
-            <Ionicons name="ios-microphone" size={75}/>
+            <Ionicons name="ios-microphone" size={100}/>
             <Button 
                 title={(this.state.isStopwatchStart==true) ? "Stop" : "Start"} size={55} color='red' onPress={() => this.getAlert()}></Button>
             <Stopwatch 
