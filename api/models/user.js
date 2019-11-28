6f0d4638f0cfd7f92b3ec3a292a8f58cd264d8cc
Falanke21 <1065314944@qq.com>
2019-11-27 21:58:05 -0500

    add bcrypt into user creation

--- api/models/user.js
+++ api/models/user.js
@@ -1,11 +1,13 @@
 const Sequelize = require("sequelize");
 const { sequelize } = require("../db/sequelize");
 
+const bcrypt = require('bcryptjs')
+
 const User = sequelize.define("user", {
     email: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique: true,
+        unique: true
     },
     password: {
         type: Sequelize.STRING,
@@ -16,8 +18,23 @@ const User = sequelize.define("user", {
         values: ["male", "female"]
     },
     birthday: {
-        type: Sequelize.DATE,
+        type: Sequelize.DATE
     }
+});
+
+User.addHook("beforeCreate", (user, options) => {
+    // generate salt and hash the password
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) {
+                    return reject(err);
+                }
+                user.password = hash
+                return resolve(user, options);
+            })
+        })
+    })
 });
 
 module.exports = { User };
