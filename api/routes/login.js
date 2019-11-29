547049d3fb60da00e69265bbbd36857dc45c69ab
zuoqiwx <zuoqiwchris@gmail.com>
2019-11-29 17:18:03 -0500

    Embedded JWT sending into login route as well as JWT retrieving in login page

--- api/routes/login.js
+++ api/routes/login.js
@@ -19,27 +19,41 @@ router.post("/", function(req, res, next) {
     User.findOne({
         where: { email: email }
     })
-        .then(user => {
-            // check whether the email is present in the db
-            if (user === null) {
-                console.log(`user is null. `);
-                res.status(404).send({ flag: false });
-                return;
-            }
-            console.log(`user ${email} exist!`);
+    .then(user => {
+        // check whether the email is present in the db
+        if (user === null) {
+            console.log(`user is null. `);
+            res.status(404).send({ flag: false });
+            return;
+        }
+        console.log(`user ${email} exist!`);
 
-            // TODO CHECK PASSWORD
-            bcrypt.compare(password, user.password, (err, isSame) => {
-                if (isSame) {
-                    res.send({ flag: true, user: user });
-                } else {
-                    res.status(400).send({ flag: false });
-                }
-            });
-        })
-        .catch(err => {
-            res.status(500).send({ flag: false, error: err });
+        // TODO CHECK PASSWORD
+        bcrypt.compare(password, user.password, (err, isSame) => {
+            if (isSame) {
+                createJWT({
+                    id: user._id,
+                    email: user.email
+                })
+                .then(token => {
+                    console.log(token);
+                    res.send({
+                        flag: true,
+                        user: user,
+                        token: token
+                    });
+                })
+                .catch(error => {
+                    res.status(500).send({ flag: false });
+                });
+            } else {
+                res.status(400).send({ flag: false });
+            }
         });
+    })
+    .catch(err => {
+        res.status(500).send({ flag: false, error: err });
+    });
 });
 
 module.exports = router;
--- sickkids_uroflow/LoginPage.js
+++ sickkids_uroflow/LoginPage.js
@@ -23,22 +23,23 @@ class LoginPage extends React.Component {
         this.setState({ password: text });
     };
 
-    handleFeedback(statusCode) {
-        if (statusCode == 200) {
+    handleFeedback(status, body) {
+        const token = body.token;
+        console.log(body);
+        if (status == 200) {
             this.props.navigation.navigate("History");
             // setJWT(token) // TODO: add param
-        } else if (statusCode == 404) {
+        } else if (status == 404) {
             alert("404: User not found");
             console.log("404: User not found");
-        } else if (statusCode == 400) {
+        } else if (status == 400) {
             alert("400: Password entered not correct");
             console.log("400: Password entered not correct");
         }
     }
 
-    login_pressed = () => {
-        // const data_base_url = 'http://' + ip_address + ':3001/login';
-
+    loginPressed = () => {
+        let status = null;
         const data_base_url = `http://${manifest.debuggerHost
             .split(`:`)
             .shift()
@@ -54,7 +55,15 @@ class LoginPage extends React.Component {
                 email: this.state.email,
                 password: this.state.password
             })
-        }).then(response => this.handleFeedback(response.status));
+        })
+        .then(result => {
+            status = result.status;
+            return result.json();
+        })
+        .then(body => this.handleFeedback(status, body))
+        .catch(error => {
+            console.log(error);
+        });
     };
 
     render() {
@@ -82,7 +91,7 @@ class LoginPage extends React.Component {
                     autoCapitalize="none"
                     onChangeText={this.handlePassword}
                 />
-                <Button onPress={this.login_pressed} title="Login" />
+                <Button onPress={this.loginPressed} title="Login" />
             </View>
         );
     }
