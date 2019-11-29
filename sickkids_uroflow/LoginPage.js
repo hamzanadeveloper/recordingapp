a88a2cb5fb056691314293f5e323cff28c9f22c9
zuoqiwx <zuoqiwchris@gmail.com>
2019-11-29 14:51:51 -0500

    Embedded JWT storage mechanism in login page primarily

--- sickkids_uroflow/LoginPage.js
+++ sickkids_uroflow/LoginPage.js
@@ -1,53 +1,85 @@
 import React from "react";
-import { Text, View, Button, StyleSheet, TextInput} from "react-native";
+import {
+    Text,
+    View,
+    Button,
+    StyleSheet,
+    TextInput,
+    AsyncStorage
+} from "react-native";
 
-import config from './config.json'
+import config from "./config.json";
 
 class LoginPage extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            email: 'default_e',
-            password: 'default_p'
-         };
+            email: "default_e",
+            password: "default_p"
+        };
+        // this.checkJWT();
     }
 
-     handleEmail = (text) => {
-        this.setState({ email: text })
-     }
-
-     handlePassword = (text) => {
-        this.setState({ password: text })
-     }
-
-     handleFeedback(statusCode){
-        if (statusCode == 200){
-            this.props.navigation.navigate("History");
-        } else if (statusCode == 404){
-            alert("404: User not found")
-            console.log("404: User not found")
-        } else if (statusCode == 400){
-            alert("400: Password entered not correct")
-            console.log("400: Password entered not correct")
+    getJWT = async () => {
+        try {
+            const result = await AsyncStorage.getItem("jwt");
+            return result;
+        } catch (error) {
+            return null;
         }
-     }
+    };
 
-     login_pressed = () => {
+    setJWT = async token => {
+        try {
+            await AsyncStorage.setItem("jwt", token);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    };
+
+    checkJWT = () => {
+        if (this.getJWT() !== null) {
+            this.props.navigation.navigate("History");
+        }
+    };
+
+    handleEmail = text => {
+        this.setState({ email: text });
+    };
+
+    handlePassword = text => {
+        this.setState({ password: text });
+    };
+
+    handleFeedback(statusCode) {
+        if (statusCode == 200) {
+            this.props.navigation.navigate("History");
+        } else if (statusCode == 404) {
+            alert("404: User not found");
+            console.log("404: User not found");
+        } else if (statusCode == 400) {
+            alert("400: Password entered not correct");
+            console.log("400: Password entered not correct");
+        }
+    }
+
+    login_pressed = () => {
         // ip_address = '100.64.166.191';
         ip_address = config.ip_address;
-        const data_base_url = 'http://' + ip_address + ':3001/login';
-         fetch(data_base_url, {
-             method: "POST",
-             headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
+        const data_base_url = "http://" + ip_address + ":3001/login";
+        fetch(data_base_url, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({
                 email: this.state.email,
-                password: this.state.password,
-            }),
-         }).then((response) => this.handleFeedback(response.status));
-     }
+                password: this.state.password
+            })
+        }).then(response => this.handleFeedback(response.status));
+    };
 
     render() {
         return (
@@ -58,22 +90,23 @@ class LoginPage extends React.Component {
                     justifyContent: "center"
                 }}
             >
-                <Text style={{ fontSize: 30, marginBottom: 30}}>Welcome to Uroflow</Text>
-                <TextInput style={styles.input}
-                    placeholder = "Email"
-                    autoCapitalize = "none"
-                    onChangeText = {this.handleEmail}
+                <Text style={{ fontSize: 30, marginBottom: 30 }}>
+                    Welcome to Uroflow
+                </Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    autoCapitalize="none"
+                    onChangeText={this.handleEmail}
                 />
-                <TextInput style={styles.input}
+                <TextInput
+                    style={styles.input}
                     secureTextEntry={true}
-                    placeholder = "Password"
-                    autoCapitalize = "none"
-                    onChangeText = {this.handlePassword}
+                    placeholder="Password"
+                    autoCapitalize="none"
+                    onChangeText={this.handlePassword}
                 />
-                <Button
-                    onPress={this.login_pressed}
-                    title="Login"
-                />
+                <Button onPress={this.login_pressed} title="Login" />
             </View>
         );
     }
@@ -83,19 +116,19 @@ export default LoginPage;
 
 const styles = StyleSheet.create({
     input: {
-       margin: 15,
-       height: 40,
-       width: 230,
-       borderColor: "steelblue",
-       borderWidth: 1,
-       paddingLeft: 15
+        margin: 15,
+        height: 40,
+        width: 230,
+        borderColor: "steelblue",
+        borderWidth: 1,
+        paddingLeft: 15
     },
     submitButton: {
-       padding: 10,
-       margin: 15,
-       height: 40,
+        padding: 10,
+        margin: 15,
+        height: 40
     },
-    submitButtonText:{
-       color: 'white'
+    submitButtonText: {
+        color: "white"
     }
- })
+});
