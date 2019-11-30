2169cc1cab8705e80f0fcab6aed4645a3c89730c
zuoqiwx <zuoqiwchris@gmail.com>
2019-11-29 21:55:10 -0500

    Now JWT will be removed locally when logging out

--- sickkids_uroflow/ProfilePage.js
+++ sickkids_uroflow/ProfilePage.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import profileImg from "./assets/profile.jpg" this dependency is not pushed zyb
-import { TextInput, Button, Alert, ScrollView } from 'react-native';
+import { TextInput, Button, Alert, ScrollView } from "react-native";
 import {
     StyleSheet,
     Text,
     View,
     Image,
     TouchableOpacity as TouchableHighlight
-} from 'react-native';
+} from "react-native";
 
+import { removeJWT } from "./utils/auth";
 
 function UselessTextInput() {
-    const [value, onChangeText] = React.useState('Name');
+    const [value, onChangeText] = React.useState("Name");
     return (
         <TextInput
-            style={{ height: 40, borderColor: 'black' }}
+            style={{ height: 40, borderColor: "black" }}
             onChangeText={text => onChangeText(text)}
             value={value}
             autoFocus={true}
@@ -25,76 +26,170 @@ function UselessTextInput() {
     );
 }
 
-
-
-
 export default class Profile extends Component {
     constructor(props) {
-        super(props)
-        this.state = { Name: "Will L.",
-                        showName: false,
-                        Gender: "Male",
-                        Age: 20,
-                        Height: "180",
-                        showHeight: false,
-                        Birthday: "1999/11/14",
-                        showBirthday: false }
-      }
-    
+        super(props);
+        this.state = {
+            Name: "Will L.",
+            showName: false,
+            Gender: "Male",
+            Age: 20,
+            Height: "180",
+            showHeight: false,
+            Birthday: "1999/11/14",
+            showBirthday: false
+        };
+    }
+
     logOut = () => {
         return Alert.alert(
-            'Logging Out',
-            'All unsaved information will be lost',
+            "Logging Out",
+            "All unsaved information will be lost",
             [
-              {
-                text: 'Cancel',
-                onPress: () => console.log('Cancel Pressed'),
-                style: 'cancel',
-              },
-              {text: 'Log Out', onPress: () => this.props.navigation.navigate("Login")},
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                {
+                    text: "Log Out",
+                    onPress: () => {
+                        removeJWT().then(res => console.log(`JWT removed: ${res}`));
+                        this.props.navigation.navigate("Login");
+                    }
+                }
             ],
-            {cancelable: false},
-          );
-    }
+            { cancelable: false }
+        );
+    };
 
     render() {
         return (
             <View>
-            <ScrollView style={styles.scrollView}>
-                <View style={styles.header}></View>
-                <Image style={styles.avatar} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
-                <Text style={styles.text}>press and hold field to edit</Text> 
-                <View style={styles.bodyContent}>
-                    <TouchableHighlight style={styles.buttonContainer} onLongPress={()=>{this.setState({showName: !this.state.showName})}}>
-                        <Text style={{color: "white"}}>Username: {this.state.Name}</Text>
-                    </TouchableHighlight>
-                    {this.state.showName ? <View style={styles.editinfo}><TextInput style={styles.textInput} 
-                                            onChangeText={(text) => this.setState({Name: text})} value={this.state.Name}/>
-                                            <Button title="Save" onPress={()=>{this.setState({showName: !this.state.showName})}}/></View>
-                    : null}
-                    <TouchableHighlight style={styles.buttonContainer} onLongPress={()=>{this.setState({showBirthday: !this.state.showBirthday})}}>
-                        <Text style={{color: "white"}}>Birthday: {this.state.Birthday}</Text>
-                    </TouchableHighlight>
-                    {this.state.showBirthday ? <View style={styles.editinfo}><TextInput style={styles.textInput} 
-                                            onChangeText={(text) => this.setState({Birthday: text})} value={this.state.Birthday}/>
-                                            <Button title="Save" onPress={()=>{this.setState({showBirthday: !this.state.showBirthday})}}/></View>
-                    : null}
-                    <TouchableHighlight style={styles.buttonContainer} onLongPress={()=>{this.setState({showHeight: !this.state.showHeight})}}>
-                        <Text style={{color: "white"}}>Height(cm): {this.state.Height}</Text>
-                    </TouchableHighlight>
-                    {this.state.showHeight ? <View style={styles.editinfo}><TextInput style={styles.textInput} 
-                                            onChangeText={(text) => this.setState({Height: text})} value={this.state.Height}/>
-                                            <Button title="Save" onPress={()=>{this.setState({showHeight: !this.state.showHeight})}}/></View>
-                    : null}
-                    <TouchableHighlight style={styles.buttonContainer}>
-                        <Text style={{color: "white"}}>Gender: {this.state.Gender}</Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight style={styles.buttonContainer}>
-                        <Text style={{color: "white"}}>Age: {this.state.Age}</Text>
-                    </TouchableHighlight>
-                    <Button title= "Log Out" style={styles.buttonContainer} onPress={this.logOut}/>
-                </View>
-            </ScrollView>
+                <ScrollView style={styles.scrollView}>
+                    <View style={styles.header}></View>
+                    <Image
+                        style={styles.avatar}
+                        source={{
+                            uri:
+                                "https://bootdey.com/img/Content/avatar/avatar6.png"
+                        }}
+                    />
+                    <Text style={styles.text}>
+                        press and hold field to edit
+                    </Text>
+                    <View style={styles.bodyContent}>
+                        <TouchableHighlight
+                            style={styles.buttonContainer}
+                            onLongPress={() => {
+                                this.setState({
+                                    showName: !this.state.showName
+                                });
+                            }}
+                        >
+                            <Text style={{ color: "white" }}>
+                                Username: {this.state.Name}
+                            </Text>
+                        </TouchableHighlight>
+                        {this.state.showName ? (
+                            <View style={styles.editinfo}>
+                                <TextInput
+                                    style={styles.textInput}
+                                    onChangeText={text =>
+                                        this.setState({ Name: text })
+                                    }
+                                    value={this.state.Name}
+                                />
+                                <Button
+                                    title="Save"
+                                    onPress={() => {
+                                        this.setState({
+                                            showName: !this.state.showName
+                                        });
+                                    }}
+                                />
+                            </View>
+                        ) : null}
+                        <TouchableHighlight
+                            style={styles.buttonContainer}
+                            onLongPress={() => {
+                                this.setState({
+                                    showBirthday: !this.state.showBirthday
+                                });
+                            }}
+                        >
+                            <Text style={{ color: "white" }}>
+                                Birthday: {this.state.Birthday}
+                            </Text>
+                        </TouchableHighlight>
+                        {this.state.showBirthday ? (
+                            <View style={styles.editinfo}>
+                                <TextInput
+                                    style={styles.textInput}
+                                    onChangeText={text =>
+                                        this.setState({ Birthday: text })
+                                    }
+                                    value={this.state.Birthday}
+                                />
+                                <Button
+                                    title="Save"
+                                    onPress={() => {
+                                        this.setState({
+                                            showBirthday: !this.state
+                                                .showBirthday
+                                        });
+                                    }}
+                                />
+                            </View>
+                        ) : null}
+                        <TouchableHighlight
+                            style={styles.buttonContainer}
+                            onLongPress={() => {
+                                this.setState({
+                                    showHeight: !this.state.showHeight
+                                });
+                            }}
+                        >
+                            <Text style={{ color: "white" }}>
+                                Height(cm): {this.state.Height}
+                            </Text>
+                        </TouchableHighlight>
+                        {this.state.showHeight ? (
+                            <View style={styles.editinfo}>
+                                <TextInput
+                                    style={styles.textInput}
+                                    onChangeText={text =>
+                                        this.setState({ Height: text })
+                                    }
+                                    value={this.state.Height}
+                                />
+                                <Button
+                                    title="Save"
+                                    onPress={() => {
+                                        this.setState({
+                                            showHeight: !this.state.showHeight
+                                        });
+                                    }}
+                                />
+                            </View>
+                        ) : null}
+                        <TouchableHighlight style={styles.buttonContainer}>
+                            <Text style={{ color: "white" }}>
+                                Gender: {this.state.Gender}
+                            </Text>
+                        </TouchableHighlight>
+                        <TouchableHighlight style={styles.buttonContainer}>
+                            <Text style={{ color: "white" }}>
+                                Age: {this.state.Age}
+                            </Text>
+                        </TouchableHighlight>
+                        <Button
+                            title="Log Out"
+                            style={styles.buttonContainer}
+                            onPress={this.logOut}
+                        />
+                    </View>
+                </ScrollView>
             </View>
         );
     }
@@ -106,7 +201,7 @@ Profile.navigationOptions = {
 const styles = StyleSheet.create({
     header: {
         backgroundColor: "steelblue",
-        height: 140,
+        height: 140
     },
     avatar: {
         width: 130,
@@ -115,24 +210,23 @@ const styles = StyleSheet.create({
         borderWidth: 4,
         borderColor: "white",
         marginBottom: 10,
-        alignSelf: 'center',
-        position: 'absolute',
+        alignSelf: "center",
+        position: "absolute",
         marginTop: 80
     },
     body: {
-        marginTop: 50,
+        marginTop: 50
     },
     bodyContent: {
         flex: 3,
-        alignItems: 'center',
+        alignItems: "center",
         padding: 30,
         flexDirection: "column"
-        
     },
     inputContent: {
         flex: 2,
-        flexDirection: 'row',
-        alignItems: 'center',
+        flexDirection: "row",
+        alignItems: "center"
     },
     info: {
         fontSize: 16,
@@ -143,34 +237,34 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: "#696969",
         marginTop: 10,
-        textAlign: 'center'
+        textAlign: "center"
     },
     buttonContainer: {
         height: 45,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
         marginBottom: 20,
         width: 250,
         borderRadius: 20,
-        backgroundColor: "steelblue",
+        backgroundColor: "steelblue"
     },
     text: {
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
         marginTop: 70,
         fontSize: 17,
-        textAlign: 'center'
+        textAlign: "center"
     },
     textInput: {
         height: 40,
-        borderColor: 'gray',
+        borderColor: "gray",
         borderWidth: 1,
-        textAlign: 'center'
+        textAlign: "center"
     },
     editinfo: {
         width: 200,
-        height: 75,
+        height: 75
     },
     scrollView: {
         margin: 5
