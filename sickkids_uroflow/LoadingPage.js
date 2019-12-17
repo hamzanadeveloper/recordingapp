f54af5d7a860a4940d9afedbcd28af614534eb7e
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-12-17 13:22:34 -0500

    Removed the old authentication

--- sickkids_uroflow/LoadingPage.js
+++ sickkids_uroflow/LoadingPage.js
@@ -1,51 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
-import Constants from "expo-constants";
-const { manifest } = Constants;
-import config from "./config.json";
-const url = config.url;
-
-import { getJWT, removeJWT } from "./utils/auth";
 
 class LoadingPage extends React.Component {
     constructor(props) {
         super(props);
-        // removeJWT(); // clear jwt field for debugging purpose
-        // checks JWT before
-        getJWT()
-            .then(token => {
-                console.log("LoadingPage: checking JWT");
-                return this.checkJWT(token);
-            })
-            .then(res => {
-                if (res.status === 200) {
-                    console.log(
-                        "LoadingPage: valid JWT, redirect to history page"
-                    );
-                    this.props.navigation.navigate("Record");
-                } else {
-                    console.log(
-                        "LoadingPage: invalid JWT, redirect to login page"
-                    );
-                    this.props.navigation.navigate("Login");
-                }
-            });
-    }
-
-    checkJWT(token) {
-        const data_base_url = url
-            ? `${url}/verify`
-            : `http://${manifest.debuggerHost
-                  .split(`:`)
-                  .shift()
-                  .concat(`:3001/verify`)}`; // Switch to the route you want to use
-        return fetch(data_base_url, {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json"
-            }
-        });
+        this.props.navigation.navigate("Login");
     }
 
     render() {
