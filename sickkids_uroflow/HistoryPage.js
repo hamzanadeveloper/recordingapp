a7d076c97e72e47b26a6912217341dc2efa6a488
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-12-20 11:14:23 -0500

    Empty text on recording history page

--- sickkids_uroflow/HistoryPage.js
+++ sickkids_uroflow/HistoryPage.js
@@ -17,20 +17,27 @@ function HistoryPage(props) {
 
     return (
         <SafeAreaView style={styles.container}>
-            {history.map(recording => (
-              <TouchableHighlight onPress={() => props.navigation.navigate('Replay', { recording })}>
-                  <View style={styles.itemContainer}>
-                      <View style={styles.itemTop}>
-                              <Text style={{fontSize: 22, fontFamily: "Avenir-Heavy", marginLeft: 15}}>{recording.description}</Text>
-                              <Text style={{fontSize: 22, fontFamily: "Avenir-Light", marginRight: 15}}>{recording.length}</Text>
-                      </View>
-                      <View style={styles.itemBottom}>
-                          <Text style={{fontSize: 14, fontFamily: "Avenir", marginLeft: 15}}>{formatDistance(new Date(recording.createdAt), new Date())} ago</Text>
-                          <Ionicons name="ios-arrow-dropright" size={18} color="#64b5f6" style={{marginRight: 15}} />
-                      </View>
-                  </View>
-              </TouchableHighlight>
-            ))}
+            { history.length == 0 ?
+              <View style={{display: 'flex', top: '10%'}}>
+                  <Text style={{fontFamily: 'Avenir-Heavy', fontSize: 16, textAlign: 'center'}}>Looks like you don't have any recordings. Press the Record tab and make some recordings!</Text>
+              </View>
+              :
+              history.map(recording => (
+                    <TouchableHighlight onPress={() => props.navigation.navigate('Replay', { recording })}>
+                        <View style={styles.itemContainer}>
+                            <View style={styles.itemTop}>
+                                <Text style={{fontSize: 22, fontFamily: "Avenir-Heavy", marginLeft: 15}}>{recording.description}</Text>
+                                <Text style={{fontSize: 22, fontFamily: "Avenir-Light", marginRight: 15}}>{recording.length}</Text>
+                            </View>
+                            <View style={styles.itemBottom}>
+                                <Text style={{fontSize: 14, fontFamily: "Avenir", marginLeft: 15}}>{formatDistance(new Date(recording.createdAt), new Date())} ago</Text>
+                                <Ionicons name="ios-arrow-dropright" size={18} color="#64b5f6" style={{marginRight: 15}} />
+                            </View>
+                        </View>
+                    </TouchableHighlight>
+                  ))
+            }
+
         </SafeAreaView>
     )
 }
